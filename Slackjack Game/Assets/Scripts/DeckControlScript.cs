355b234ca0545cd93699816f8e07c47fdd1cf1fe
Meta <metapandas@gmail.com>
2021-09-21 16:44:50 -0400

    Created Deck Control Object & Card Sprites Array
    -Created Deck Control Object called "Deck" with
     DeckControlScript
    
    -In DeckControlScript created cardSprites array to hold all face values
     and red card back
    
    -Element 0 = Card back
     Elements 1-52 = Cards listed in order by face value
     Suit order: Clubs, Diamonds, Hearts, Spades

---
+++ Slackjack Game/Assets/Scripts/DeckControlScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckControlScript : MonoBehaviour
+{
+    public Sprite[] cardSprites;
+    int[] cardValues = new int[53];
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
