101ca178358653fa31dfa06ff7d05ce99e00be08
Meta <metapandas@gmail.com>
2021-09-23 17:39:59 -0400

    Added Card Values to DeckControlScript

--- Slackjack Game/Assets/Scripts/DeckControlScript.cs
+++ Slackjack Game/Assets/Scripts/DeckControlScript.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class DeckControlScript : MonoBehaviour
 {
+    //Array to hold card sprites
     public Sprite[] cardSprites;
+
+    //Array to hold card values;
     int[] cardValues = new int[53];
 
     void Start()
@@ -12,9 +15,64 @@ public class DeckControlScript : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    //Hard Coded Values to match images of card sprites in cardSprites Array.
+    //Suits order is Club(1-13), Diamonds(14-26), Hearts(27-39), Spades(4-52).
+    //Loop can be created later if neccesary to apply values, but since values
+    //will never change, considered it unnecessary. 
+    void AssignCardValues()
     {
-        
+        cardValues[1] = 1;
+        cardValues[2] = 2;
+        cardValues[3] = 3;
+        cardValues[4] = 4;
+        cardValues[5] = 5;
+        cardValues[6] = 6;
+        cardValues[7] = 7;
+        cardValues[8] = 8;
+        cardValues[9] = 9;
+        cardValues[10] = 10;
+        cardValues[11] = 10;
+        cardValues[12] = 10;
+        cardValues[13] = 10;
+        cardValues[14] = 1;
+        cardValues[15] = 2;
+        cardValues[16] = 3;
+        cardValues[17] = 4;
+        cardValues[18] = 5;
+        cardValues[19] = 6;
+        cardValues[20] = 7;
+        cardValues[21] = 8;
+        cardValues[22] = 9;
+        cardValues[23] = 10;
+        cardValues[24] = 10;
+        cardValues[25] = 10;
+        cardValues[26] = 10;
+        cardValues[27] = 1;
+        cardValues[28] = 2;
+        cardValues[29] = 3;
+        cardValues[30] = 4;
+        cardValues[31] = 5;
+        cardValues[32] = 6;
+        cardValues[33] = 7;
+        cardValues[34] = 8;
+        cardValues[35] = 9;
+        cardValues[36] = 10;
+        cardValues[37] = 10;
+        cardValues[38] = 10;
+        cardValues[39] = 10;
+        cardValues[40] = 1;
+        cardValues[41] = 2;
+        cardValues[42] = 3;
+        cardValues[43] = 4;
+        cardValues[44] = 5;
+        cardValues[45] = 6;
+        cardValues[46] = 7;
+        cardValues[47] = 8;
+        cardValues[48] = 9;
+        cardValues[49] = 10;
+        cardValues[50] = 10;
+        cardValues[51] = 10;
+        cardValues[52] = 10;
+
     }
 }
--- Slackjack Game/Assets/Scripts/GameManager.cs
+++ Slackjack Game/Assets/Scripts/GameManager.cs
@@ -1,20 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-   
-    
+
+    //Buttons
+    public Button dealBtn_prototype;
+    public Button standBtn_prototype;
+    public Button hitBtn_prototype;
+    public Button shuffleBtn_prototype;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Adding listeners
+        dealBtn_prototype.onClick.AddListener(() => DealClicked());
+        standBtn_prototype.onClick.AddListener(() => StandClicked());
+        hitBtn_prototype.onClick.AddListener(() => HitClicked());
+        shuffleBtn_prototype.onClick.AddListener(() => ShuffleClicked());
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Deck Methods
+    private void ShuffleClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HitClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void StandClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DealClicked()
+    {
+        throw new NotImplementedException();
     }
 }
